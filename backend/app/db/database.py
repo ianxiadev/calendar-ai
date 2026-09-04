3a3a6a0bfcf83cc68002150e86b7dabd4dbc5b3e
Ian Xia <IXWinter25@gmail.com>
2026-09-03 20:20:55 -0700

    Added SQList database with AQLAlchemy, connecting creating events (POST) and reading events(GET) to a real, persistent database.

---
+++ backend/app/db/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer, String
+
+DATABASE_URL = "sqlite:///./calender.db" #Tells SQLAlchemy what kind of database to connect to and where calender.db exists in the repo
+
+engine = create_engine(DATABASE_URL) # This engine is SQLAlchemy's core connection object managing communication wih the database file
+
+SessionLocal = sessionmaker(bind=engine) # A session is a emporary workspace for doing database operations. We need sessions so operations from different users/requests do not interfere with one another
+
+Base = declarative_base() # Base class that all table definitions inherit from, represents the shape of a database tableoka
+
+class EventModel(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key = True)
+    title = Column(String)
+    start = Column(String)
+    end = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+# Opens a session, hands session to the route to use, and once route function is done, execution comes back and closes the session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI # Pull out FastAPI class inside fastapi package
+from pydantic import BaseModel
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.db.database import SessionLocal, EventModel, get_db
+
+class Event(BaseModel): # New class event that inherits from BaseModel, receiving all Pydantic's validation abilities 
+    title: str
+    start: str
+    end: str
+
 
 app = FastAPI() # Create an instance of FastAPI class as object app
 
@@ -7,8 +17,13 @@ def read_root():
     return {"message": "Calender API is running"}
 
 @app.get("/events")
-def list_events():
-    return [
-        {"id": 1, "title": "CS Lecture", "start": "2026-09-05T14:00:00", "end": "2026-09-05T15:20:00"},
-        {"id": 2, "title": "Study for exam", "start": "2026-09-07T19:00:00", "end": "2026-09-07T21:00:00"},
-    ]
+def list_events(db: Session = Depends(get_db)):
+    return db.query(EventModel).all() # Look at events table and get every row (generates real SQL under the hood), returning Python objects representing each row
+
+@app.post("/events")
+def create_event(event: Event, db: Session = Depends(get_db)): # Depends(get_db) tells FastAPI to first call get_db() and pass its output as db parameter
+    new_event = EventModel(title=event.title, start=event.start, end=event.end)
+    db.add(new_event)
+    db.commit()
+    db.refresh(new_event) # database fills in detailed you didn't provide such as the auto-generated id number
+    return new_event
