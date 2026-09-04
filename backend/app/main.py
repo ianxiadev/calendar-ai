195599b935c0a60079e12415ed2ed6d8f8fb49cc
Ian Xia <IXWinter25@gmail.com>
2026-09-04 14:03:44 -0700

    Added PATCH operation for more convenient event updating.

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,11 +4,17 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from app.db.database import SessionLocal, EventModel, get_db
 from fastapi import HTTPException
+from typing import Optional
 
 class Event(BaseModel): # New class event that inherits from BaseModel, receiving all Pydantic's validation abilities 
     title: str
     start: str
     end: str
+
+class EventUpdate(BaseModel):
+    title: Optional[str] = None
+    start: Optional[str] = None
+    end: Optional[str] = None
 
 
 app = FastAPI() # Create an instance of FastAPI class as object app
@@ -53,3 +59,18 @@ def delete_event(event_id: int, db: Session = Depends(get_db)):
     db.delete(db_event)
     db.commit()
     return {"detail": f"Event '{event_title}' deleted"}
+
+@app.patch("/events/{event_id}")
+def patch_event(event_id: int, event: EventUpdate, db: Session = Depends(get_db)):
+    
+    db_event = db.query(EventModel).filter(EventModel.id == event_id).first()
+    if db_event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    
+    update_data = event.dict(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(db_event, field, value)
+    
+    db.commit()
+    db.refresh(db_event)
+    return db_event
